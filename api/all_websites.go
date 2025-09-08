aab41c6d8dbf0cf0fc071425ddbe2f133303e2d9
Rowan <104677625+rowandevving@users.noreply.github.com>
2025-09-08 21:26:51 +0100

    add cool people page and api endpoint

---
+++ api/all_websites.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/jackc/pgx/v5"
+)
+
+type Website struct {
+	Data  WebsiteData `json:"data"`
+	Title string      `json:"title"`
+}
+
+type WebsiteData struct {
+	Domain string `json:"domain"`
+	Colour string `json:"colour"`
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		log.Printf("Couldn't connect to database: %v", err)
+		return
+	}
+	defer conn.Close(context.Background())
+
+	rows, err := conn.Query(context.Background(), "select domain, colour from cool_people where approved = true")
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		log.Printf("Couldn't query database: %v", err)
+		return
+	}
+	websiteDatas, err := pgx.CollectRows(rows, pgx.RowToStructByName[WebsiteData])
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		log.Printf("Error: %v", err)
+		return
+	}
+
+	var websites []Website
+
+	for _, websiteData := range websiteDatas {
+		u, err := url.Parse(websiteData.Domain)
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			log.Printf("Error: %v", err)
+			return
+		}
+		u.Scheme = "https"
+
+		res, err := http.Get(u.String())
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			log.Printf("Error: %v", err)
+			return
+		}
+
+		doc, err := goquery.NewDocumentFromReader(res.Body)
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			log.Printf("Error: %v", err)
+			return
+		}
+
+		defer res.Body.Close()
+
+		title := doc.Find("head title").Text()
+
+		websites = append(websites, Website{websiteData, title})
+	}
+
+	data, err := json.Marshal(websites)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		log.Printf("Couldn't marshal JSON: %v", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Cache-Control", "public, max-age=300, s-maxage=86400, stale-while-revalidate=300")
+
+	fmt.Fprint(w, string(data))
+}
---
+++ api/go.mod
@@ -0,0 +1,17 @@
+module github.com/rowandevving/personal-website
+
+go 1.23.4
+
+require (
+	github.com/PuerkitoBio/goquery v1.10.3
+	github.com/jackc/pgx/v5 v5.7.5
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	golang.org/x/crypto v0.41.0 // indirect
+	golang.org/x/net v0.42.0 // indirect
+	golang.org/x/text v0.28.0 // indirect
+)
